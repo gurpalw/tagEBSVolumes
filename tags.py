b96992517ddeed2269351227cd88399e9c85d668
Gurpalw <gurpal.walia@viooh.com>
2018-11-26 15:52:05 +0000

    remove print statements and use logging instead

--- tags.py
+++ tags.py
@@ -2,12 +2,18 @@ import boto3
 import logging
 from variables import RoleArn
 from time import strftime
-logger = logging.getLogger('myapp')
-hdlr = logging.FileHandler(strftime("/var/tmp/tagEBSVolume_%H_%M_%m_%d_%Y.log"))
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
+
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh = logging.FileHandler(strftime("/var/tmp/tagEBSVolume_%H_%M_%m_%d_%Y.log"))
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 sts_client = boto3.client('sts')
 
@@ -43,12 +49,9 @@ def copythetags(instance):
             tagdictionary['Value'] = itag['Value']
             tagdictionary['Key'] = itag['Key']
             temptags.append(tagdictionary.copy())
-            print("[INFO] Copying Tag...")
             logger.info('Copying Tag...')
-            print("[INFO] Key: " + itag['Key'] + " Value: " + itag['Value'])
             logger.info("Key: " + itag['Key'] + " Value: " + itag['Value'])
         else:
-            print("[INFO]: Skipping Tag: - Key: " + itag['Key'])
             logger.info("Skipping Tag: - Key: " + itag['Key'])
     return temptags
 
@@ -59,16 +62,12 @@ for instance in instances:
     if instance.tags is not None:
         for volume in instance.volumes.all():
             if dryRun:
-                print("[INFO] " + str(volume) + " [INSTANCE] " + str(instance))
                 logger.info(str(volume) + " [INSTANCE] " + str(instance))
                 copythetags(instance)
             else:
-                print("[INFO] Tagging " + str(volume) + " attached to  " + str(instance))
                 logger.info("Tagging " + str(volume) + " attached to  " + str(instance))
                 tag = volume.create_tags(Tags=copythetags(instance))
                 count += 1
-                print("[INFO] " + str(count) + " volumes tagged.")
-                logger.info(str(count) + " volumes tagged.")
+                logger.info(str(count) + " volumes tagged. ")
     else:
-        print("[INFO] Skipping volumes attached to instance " + str(instance) + " ... no tags.")
         logger.info("Skipping volumes attached to instance " + str(instance) + " ... no tags.")
